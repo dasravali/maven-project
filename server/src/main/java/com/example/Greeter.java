4d610e6a4a6789d644fa2f0db39fdce66a7e14d4
dasravali <dasyam.ravali09@gmail.com>
2020-06-02 16:43:14 +0530

    fixed checkstyle new warning added line

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,11 +11,12 @@ public class Greeter {
   public Greeter() {  
 
   }
-  /**
-  * @param someone of the person
+  
+   /**
+   * @param someone of the person
    * @return greeting string
    */
-  public final String greet(final String someone) {
+   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
